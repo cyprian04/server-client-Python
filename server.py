ebd8b5f162a9b3c912ef37f71899e31db6b37d3e
Cyprian <Cyprian.Gibas@o2.pl>
2023-11-21 19:50:44 +0100

    initializing server_socket with port and ip address

--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 import socket
 
 PORT = 5100
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDRESS = (SERVER, PORT)
+HOST = socket.gethostbyname(socket.gethostname())
+ADDRESS = (HOST, PORT)
 
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(HOST, PORT)
+
+
+
+print("[STARTING] server is starting...")
