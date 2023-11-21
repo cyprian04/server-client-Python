067f26db6e4a2a0f9a35ffba57d4288a21742f4d
Cyprian <Cyprian.Gibas@o2.pl>
2023-11-21 22:21:59 +0100

    Made the event loop for handling new conns, and sending data by curr user

--- server.py
+++ server.py
@@ -15,3 +15,18 @@ listening_socket.listen()
 print(f"[LISTENING] Server is listening on {ADDRESS}")
 listening_socket.setblocking(False) # since we want multiple connections we set blocking on false, because we gonna block later with events
 sel.register(listening_socket, selectors.EVENT_READ, data=None) # sel.register() registers the socket to be monitored with sel.select() for the events that you’re interested in. For the listening socket, you want read events: selectors.EVENT_READ.
+
+try:
+    while True:
+        events = sel.select(timeout=None) # waiting for conns on registered listening_socket
+        for key, mask in events:
+            if key.data is None:
+                #accept_wrapper(key.fileobj) #if new connection on listening socket
+                pass
+            else:
+                #service_connection(key, mask) # if client is already connected and send smth
+                pass
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
