d3765f0c976956243c291a2b3268ae5f81853fe9
Cyprian <Cyprian.Gibas@o2.pl>
2023-11-21 20:31:13 +0100

    Added important comments

--- server.py
+++ server.py
@@ -10,10 +10,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket: # same 
     server_socket.bind(HOST, PORT)
     server_socket.listen()
     user_conn, user_addr = server_socket.accept()
-    with user_conn:
+    with user_conn: # if socket(connection with client) is open
         print(f"Connection estabilished with {user_addr}")
         while True:
             data = user_conn.recv(1024)
-            if not data:
+            if not data: # if empty then the client shut down the connection
                 break
             user_conn.sendall(data)
