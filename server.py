3c374fcbcc19f471bb91f09e86aecd1fbb2807dc
Cyprian <Cyprian.Gibas@o2.pl>
2023-11-21 20:48:30 +0100

    wrapped the starting server code in func named start

--- server.py
+++ server.py
@@ -4,16 +4,18 @@ PORT = 5500
 HOST = socket.gethostbyname(socket.gethostname())
 ADDRESS = (HOST, PORT)
 
-print("[STARTING] server is starting...")
+def start():
+    print("[STARTING] server is starting...")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket: # same as using in c#
+        server_socket.bind(ADDRESS)
+        server_socket.listen()
+        user_conn, user_addr = server_socket.accept() # creating new socket which connects server with connected client
+        with user_conn: # if socket(connection with client) is open
+            print(f"Connection estabilished with {user_addr}")
+            while True:
+                data = user_conn.recv(1024)
+                if not data: # if empty then the client shut down the connection
+                    break
+                user_conn.sendall(data)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket: # same as using in c#
-    server_socket.bind(ADDRESS)
-    server_socket.listen()
-    user_conn, user_addr = server_socket.accept()
-    with user_conn: # if socket(connection with client) is open
-        print(f"Connection estabilished with {user_addr}")
-        while True:
-            data = user_conn.recv(1024)
-            if not data: # if empty then the client shut down the connection
-                break
-            user_conn.sendall(data)
+start()
