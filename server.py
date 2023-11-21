f592a50665684719bb815a4edb47b52244543a56
Cyprian <Cyprian.Gibas@o2.pl>
2023-11-21 20:28:06 +0100

    Made the basic behaviour for listening and connecting single client

--- server.py
+++ server.py
@@ -4,9 +4,16 @@ PORT = 5100
 HOST = socket.gethostbyname(socket.gethostname())
 ADDRESS = (HOST, PORT)
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(HOST, PORT)
-
-
-
 print("[STARTING] server is starting...")
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket: # same as using in c#
+    server_socket.bind(HOST, PORT)
+    server_socket.listen()
+    user_conn, user_addr = server_socket.accept()
+    with user_conn:
+        print(f"Connection estabilished with {user_addr}")
+        while True:
+            data = user_conn.recv(1024)
+            if not data:
+                break
+            user_conn.sendall(data)
