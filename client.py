0c2116535612199bb9339cc0bee029fb031c2d40
Cyprian <Cyprian.Gibas@o2.pl>
2023-11-22 21:39:02 +0100

    Made basic setup in code for client, conn with server works

--- client.py
+++ client.py
@@ -1,12 +1,31 @@
+import sys
 import socket
+import selectors
+import types
 
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 5500  # The port used by the server
 ADDRESS = (HOST, PORT)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-    server_socket.connect(ADDRESS)
-    server_socket.sendall(b"Hello, world")
-    data = server_socket.recv(1024)
+sel = selectors.DefaultSelector()
+messages = [b"Message 1 from client.", b"Message 2 from client."]
 
-print(f"Received {data!r}")
+def start_connections():
+    server_addr = (ADDRESS)
+    for i in range(0, 2):
+        connid = i + 1
+        print(f"Starting connection {connid} to {server_addr}")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(False)
+        sock.connect_ex(server_addr)
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        data = types.SimpleNamespace(
+            connid=connid,
+            msg_total=sum(len(m) for m in messages),
+            recv_total=0,
+            messages=messages.copy(),
+            outb=b"",
+        )
+        sel.register(sock, events, data=data)
+
+start_connections()
