b118957d0168acf076f6eeef9ae1ea0900ec9ab4
Cyprian <Cyprian.Gibas@o2.pl>
2023-11-23 20:49:57 +0100

    Fixed the layout and changed the comments

--- client.py
+++ client.py
@@ -3,27 +3,21 @@ import sys
 import select
 
 def main():
-    # Create a TCP/IP socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Connect the socket to the server's address and port
     server_address = ('localhost', 8888)
     client_socket.connect(server_address)
     print(f"[CONNECTION] Connected to Server: {server_address}")
-
-    # Set the socket to non-blocking mode
     client_socket.setblocking(False)
 
-    while True:
-        # Wait for an activity on the socket or stdin
+    while True:# Wait for an activity on the socket or stdin
         try:
             readable, _, _ = select.select([client_socket], [], [], 0.1)
         except select.error as e:
+            print(str(e))
             break
 
-        for sock in readable:
-            if sock == client_socket:
-                # Receive and print messages from the server
+        for sock in readable: # readable is a list of active sockets(for reading)
+            if sock == client_socket:# Receive and print messages from the server
                 data = client_socket.recv(1024)
                 if not data:
                     print("[CLOSING] Disconnected from the server.")
@@ -31,8 +25,7 @@ def main():
                 else:
                     print("[MESSAGE] Received message: {}".format(data.decode()))
 
-        # Check if there is input from the user
-        try:
+        try: # Check if there is input from the user
             message = sys.stdin.readline()
         except KeyboardInterrupt:
             print("Exiting the client.")
